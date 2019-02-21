ff8b57f4dd65d58a8457919c2e292b72b6d71997
McNultyPT <mcnultypt@gmail.com>
2019-02-21 10:10:02 -0800

    saving changes

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shortcuts');
+    const messageOfTheDay = process.env.MOTD ||
+    'Hello World';
+    res.status(200).json({ motd: messageOfTheDay, shoutouts });
+  } catch(error) {
+    console.log('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
+
 const port = process.env.PORT || 5000
 
 server.listen(port, () => {
